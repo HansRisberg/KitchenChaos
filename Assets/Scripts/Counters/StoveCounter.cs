f7f3d80d92047e0b512728338253f3a38f9d527e
HansRisberg <123938027+HansRisberg@users.noreply.github.com>
2023-11-21 12:32:12 +0100

    main save
    update 21.11.2023

---
+++ Assets/Scripts/Counters/StoveCounter.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoveCounter : BaseCounter {
+
+
+    [SerializeField] private FryingRecipeSO[] fryingRecipeSOArray;
+
+
+}
+
+    
+
