8793f155ad403d55e0623a4222c7ee242db9ab64
HansRisberg <123938027+HansRisberg@users.noreply.github.com>
2023-12-15 15:18:06 +0100

    Main Save
    End of day 15.12.23

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -2,10 +2,21 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour {
 
+    private const string PLAYER_PREFS_EFFECTS_VOLUME = "SoundEffectsVolume";
+
     public static SoundManager Instance { get; private set; }
 
 
     [SerializeField] private AudioClipRefsSO audioClipRefsSO;
+
+    private float volume = 1f;
+
+    private void Awake() {
+        Instance = this;
+
+        volume = PlayerPrefs.GetFloat(PLAYER_PREFS_EFFECTS_VOLUME, 1f);
+    }
+
 
     private void Start() {
         DeliveryManager.Instance.OnRecipeSuccess += Delivery_OnRecipeSuccess;
@@ -16,9 +27,6 @@ public class SoundManager : MonoBehaviour {
         TrashCounter.OnAnyObjectTrashed += TrashCounter_OnAnyObjectTrashed;
     }
 
-    private void Awake() {
-        Instance = this;
-    }
 
     private void TrashCounter_OnAnyObjectTrashed(object sender, System.EventArgs e) {
         TrashCounter trashCounter = sender as TrashCounter;
@@ -53,11 +61,25 @@ public class SoundManager : MonoBehaviour {
         PlaySound(audioClipArray[Random.Range(0, audioClipArray.Length)], position, volume);
     }
 
-    private void PlaySound(AudioClip audioClip, Vector3 position, float volume = 1f) {
-        AudioSource.PlayClipAtPoint(audioClip, position, volume);
+    private void PlaySound(AudioClip audioClip, Vector3 position, float volumeMultiplier = 1f) {
+        AudioSource.PlayClipAtPoint(audioClip, position, volumeMultiplier * volume);
     }
 
     public void PlayFootstepsSound(Vector3 position, float volume) {
         PlaySound(audioClipRefsSO.footstep, position, volume);
+    }
+
+    public void ChangeVolume() {
+        volume += .1f;
+        if (volume > 1.1f) {
+            volume = 0f;
+        }
+
+        PlayerPrefs.SetFloat(PLAYER_PREFS_EFFECTS_VOLUME, volume);
+        PlayerPrefs.Save();
+    }
+
+    public float GetVolume() {
+        return volume;
     }
 }
